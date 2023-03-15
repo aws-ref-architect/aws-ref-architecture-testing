66d1cd7c29e93bed5aee36debf402ef3c6298b61
aws-ref-architect <123782877+aws-ref-architect@users.noreply.github.com>
2023-03-15 21:16:47 +0000

    Add dynamo/s3 for terraform state and default vpc

---
+++ modules/vpc.tf
@@ -0,0 +1,13 @@
+# terraform {
+#   backend "s3" {
+#     bucket         = "terraform-state-backend"
+#     key            = "terraform.tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "terraform_state"
+#   }
+# }
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/8"
+  instance_tenancy     = "default"
+}
